d2d83e7ff146baaaf9a6ba85d5f4962ba6a5873f
infamoose <daryn25000@gmail.com>
2026-03-03 15:29:33 -0500

    added comments. no comment.

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ class Book {
         BookStatus status;
     public:
         Book() {};
+        // default constructor
         Book(string t, string a, Genre g) {
             title = t;
             author = a;
@@ -30,12 +31,15 @@ class Book {
 
         virtual void displayInfo() {};
 
+        // set book status
         void setStatus(BookStatus s) {
             status = s;
         }
+        // get status
         BookStatus getStatus() const {
             return status;
         }
+        // convert genre to string
         string genreToString(Genre a) {
             if (a == Fiction) {
                 return "Fiction";
@@ -54,6 +58,7 @@ class Book {
             }
             return "ERROR";
         }
+        // equals operator, could have used it quite a lot, didnt
         bool operator==(const Book& other) const {
             if (title == other.title) {
                 if (author == other.author) {
@@ -64,17 +69,20 @@ class Book {
             }
             return false;
         }
+        // get title of book
         string getTitle() {
             return title;
         }
+        // get book type
         virtual string bookType() {
             return "None";
         }
-
+        // output book into stream
         friend ostream& operator<<(ostream& os, Book& b) {
             os << b.genreToString(b.genre) << "," << b.title << "," << b.author << "," << b.bookType() << "," << b.getSpecialData() << endl;
             return os;
         }
+        // special book data
         virtual string getSpecialData() {
             return "None";
         }
@@ -83,6 +91,7 @@ class Book {
 class EBook : public Book {
     double fileSizeMB;
     public:
+        // default constructor
         EBook(string t, string a, Genre g, double size) {
             title = t;
             author = a;
@@ -90,6 +99,7 @@ class EBook : public Book {
             fileSizeMB = size;
             setStatus(Available);
         }
+        // display info about book
         void displayInfo() override {
             cout << "--------------------------------------------------------" << endl;
             cout << title << ", by " << author << ", Genre: " << genreToString(genre) << ",  File Size: " << fileSizeMB << "MB" << endl;
@@ -100,6 +110,7 @@ class EBook : public Book {
                 cout << "Status: Available" << endl;
             }
         }
+        // genre to string, converts it.
         string genreToString(Genre a) {
             if (a == Fiction) {
                 return "Fiction";
@@ -118,10 +129,12 @@ class EBook : public Book {
             }
             return "ERROR";
         }
+        // gets the file size and rounds it. usually.
         string getSpecialData() {
             int fs = fileSizeMB*10;
             return to_string(fs/10.0);
         }
+        // get book type, ebook always.
         string bookType() {
             return "EBook";
         }
@@ -130,7 +143,7 @@ class EBook : public Book {
 class PrintedBook : public Book {
     int pageCount;
     public:
-
+        // default constructor
         PrintedBook(string t, string a, Genre g, int pages) {
             title = t;
             author = a;
@@ -138,6 +151,7 @@ class PrintedBook : public Book {
             pageCount = pages;
             setStatus(Available);
         }
+        // display info about book
         void displayInfo() override {
             cout << "--------------------------------------------------------" << endl;
             cout << title << ", by " << author << ", Genre: " << genreToString(genre) << ",  Page Count: " << pageCount << endl;
@@ -148,6 +162,7 @@ class PrintedBook : public Book {
                 cout << "Status: Available" << endl;
             }
         }
+        // convert genre to a string
         string genreToString(Genre a) const {
             if (a == Fiction) {
                 return "Fiction";
@@ -166,9 +181,11 @@ class PrintedBook : public Book {
             }
             return "ERROR";
         }
+        // get special data about book, pagecount
         string getSpecialData() {
             return to_string(pageCount);
         }
+        // get book type, always printed
         string bookType() {
             return "Printed";
         }
@@ -181,6 +198,7 @@ class Transaction {
     string date;
     string checked;
     public:
+        // default constructor
         Transaction(int pid, Book* b) {
             book = b;
             patronId = pid;
@@ -189,6 +207,7 @@ class Transaction {
             
 
         }
+        // display the transaction, as patron x checked out book y or patron x returned book y
         void displayTransaction() const {
             cout << "--------------------------------------------------------" << endl;
             if (book->getStatus() == CheckedOut) {
@@ -199,9 +218,11 @@ class Transaction {
             }
             cout << endl;
         }
+        // get the book in the transaction
         Book* getBook() {
             return book;
         }
+        // output transaction to stream
         friend ostream& operator<<(ostream& os, Transaction& t) {
             if (t.getBook()->getStatus() == CheckedOut) { 
                 os << "Book: " << t.getBook()->getTitle() << " checked out by PatronID: " << t.patronId << endl;
@@ -218,24 +239,30 @@ class Patron {
     int id;
     vector<Book*> borrowedBooks;
     public:
+        // default constructor
         Patron(string n, int i) {
             name = n;
             id = i;
         }
+        // get the patron id
         int getID() {
             return id;
         }
+        // borrow the book, so set it to checked out
         void borrowBook(Book* b) {
             // borrow dat thang
             b->setStatus(CheckedOut);
         }
+        // return the book, set it to available
         void returnBook(Book* b) {
             b->setStatus(Available);
         }
+        // display the patron
         void displayPatron() const {
             cout << "--------------------------------------------------------" << endl;
             cout << name << ": " << id << endl << endl;
         }
+        // also could have used this but didn't. checks if the patron is equal to another patron
         bool operator==(const Patron& other) const {
             if (name == other.name) {
                 if (id == other.id) {
@@ -244,6 +271,7 @@ class Patron {
             }
             return false;
         }
+        // output the patron to text file
         friend ostream& operator<<(ostream& os, const Patron& p) {
             os << p.id << "," << p.name << endl;
             return os;
@@ -255,7 +283,9 @@ class Library {
     vector<Patron> patrons;
     vector<Transaction> transactions;
     public:
+        // default constructors
         ~ Library() {}
+        // load data from library
         void loadData() {
             // loop throguht the inputs.txt an then put all the books in a list
             ifstream inFS("inputs.txt");
@@ -263,6 +293,7 @@ class Library {
             string line;
             vector<string> bookData;
             
+            // get each line and add it to row data, and then process it into books
             while (getline(inFS, line)) {
                 rowData.push_back(line);
                 stringstream ss(line);
@@ -278,11 +309,13 @@ class Library {
                 }
                 bookData.clear();
             }
-
+            // clear for reuse
             rowData.clear();
             inFS.close();
             inFS.open("patrons.txt");
 
+            
+            // get each line and process them into patrons. clear after.
             while(getline(inFS, line)) {
                 rowData.push_back(line);
                 stringstream ss(line);
@@ -295,6 +328,7 @@ class Library {
             }
 
         }
+        // change string to a genre
         Genre stringToGenre(string a) {
             if (a == "Fiction") {
                 return Fiction;
@@ -316,36 +350,41 @@ class Library {
             }
             return Fiction; // Error handling :D
         }
-
+        // save the data to the files
         void saveData() {
             // update the text files !!!
             ofstream outFS("inputs.txt");
             for (Book* b:books) {
                 outFS << *b;
             }
+            // close  files and open next
             outFS.close();
             outFS.open("patrons.txt");
             for (Patron p:patrons) {
                 outFS << p;
             }
+            // close files and open next
             outFS.close();
             outFS.open("transactions.txt");
             for (Transaction t:transactions) {
                 outFS << t;
             }
         }
+        // add book to books
         void addBook(Book* b) {
             books.push_back(b);
         }
+        // add patron to patrons
         void addPatron(const Patron& p) {
             patrons.push_back(p);
         }
+        // check out a book
         void checkoutBook(int patronId, string title) {
             for (Book* b:books) {
                 if (title == b->getTitle()) { // if the title matches
                     cout << "Found Book..." << endl;
                     if (b->getStatus() == CheckedOut) {
-                        cout << "Book Checked out Already." << endl;
+                        cout << "Book Checked out Already." << endl; // check to make sure book isn't checked out
                         break;
                     }
                     for (Patron p:patrons) { // loop through patrons
@@ -361,12 +400,13 @@ class Library {
                 }
             }
         }
+        // return book
         void returnBook(int patronId, string title) {
-            for (Transaction t:transactions) {
-                if (t.getBook()->getTitle() == title) {
-                    for (Patron p:patrons) {
-                        if (p.getID() == patronId) {
-                            p.returnBook(t.getBook());
+            for (Transaction t:transactions) { // for each transaction
+                if (t.getBook()->getTitle() == title) { // if the title matches
+                    for (Patron p:patrons) { // for each patron
+                        if (p.getID() == patronId) { // if the id matches
+                            p.returnBook(t.getBook()); // return book
                             t.displayTransaction();
                             break;
                         }
@@ -375,11 +415,12 @@ class Library {
                 }
             }
         }
-        void displayBooks() const {
+        void displayBooks() const { // display all books
             for (Book* b:books) {
                 b->displayInfo();
             }
         }
+        // display all patrons
         void displayPatrons() const {
             for (Patron p:patrons) {
                 p.displayPatron();
@@ -387,7 +428,7 @@ class Library {
         }
     };
 
-
+// print function to print at start of program
 void printOnStart() {
     cout << endl;
     cout << "1. Display Books" << endl;
@@ -397,7 +438,7 @@ void printOnStart() {
     cout << "5. Add Patrons" << endl;
     cout << "6. Quit" << endl;
 }
-
+// get functions to simplify readability.
 string getStringInput(string message) {
     cout << message << ": ";
     string temp;
@@ -422,6 +463,7 @@ double getDoubleInput(string message) {
     cout << endl;
     return temp;
 }
+// string to genre, again. Don't ask.
 Genre stringToGenre(string a) {
     if (a == "Fiction") {
         return Fiction;
@@ -446,13 +488,13 @@ Genre stringToGenre(string a) {
 
 
 int main() {
-    Library library;
+    Library library; // make a library class
     try {
-        library.loadData();
-        printOnStart();
+        library.loadData(); // load the data
+        printOnStart(); // print actions
         int temp;
-        temp = getIntInput("Action");
-        while (temp!=6) {
+        temp = getIntInput("Action"); // get action
+        while (temp!=6) { // while not quit
             if (temp==1) {
                 library.displayBooks();
             }
